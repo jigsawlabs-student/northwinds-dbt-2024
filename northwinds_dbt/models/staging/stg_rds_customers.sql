8e305b6416fe338c2b5b3c497725cc7a034eca6a
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-01-31 16:13:44 -0800

    Changed stg customers to have columns first and last
    Fixes mTdS7NOP
    
    See https://trello.com/c/mTdS7NOP/16-trello-tip-%F0%9F%8C%8Aslide-your-qs-into-this-handy-list-so-your-team-keeps-on-flowing

--- northwinds_dbt/models/staging/stg_rds_customers.sql
+++ northwinds_dbt/models/staging/stg_rds_customers.sql
@@ -2,8 +2,8 @@
 with source as ( 
     select * from public.customers
 ), renamed as (
-    select customer_id, country, split_part(contact_name, ' ', 1) as first_name,
-    split_part(contact_name, ' ', 2) as last_name 
+    select customer_id, country, split_part(contact_name, ' ', 1) as first,
+    split_part(contact_name, ' ', 2) as last
     from source
 )
 select * from renamed
